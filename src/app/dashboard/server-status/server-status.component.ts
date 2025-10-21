7b27b962d5831194d71dd3a782311810d1f04aaa
Richard Burman <richard.burman@harperjames.co.uk>
2025-10-21 14:36:22 +0100

    Refactor server status component to implement interval management and cleanup on destroy

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, Component, DestroyRef, inject, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,14 +7,15 @@ import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent implements OnInit, AfterViewInit, OnDestroy {
+export class ServerStatusComponent implements OnInit, AfterViewInit {
   currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
-  private interval?: ReturnType<typeof setInterval>;
+  // private interval?: ReturnType<typeof setInterval>;
+  private destroyRef = inject(DestroyRef)
   constructor() {}
 
   ngOnInit(): void {
     console.log('ON INIT RUNNING');
-    this.interval = setInterval(() => {
+    const interval = setInterval(() => {
       const randomValue = Math.random();
       if (randomValue < 0.5) {
         this.currentStatus = 'online';
@@ -24,14 +25,21 @@ export class ServerStatusComponent implements OnInit, AfterViewInit, OnDestroy {
         this.currentStatus = 'unknown';
       }
     }, 5000);
+
+    this.destroyRef.onDestroy(() => {
+      console.log('ON DESTROY RUNNING');
+      clearInterval(interval);
+    });
   }
 
   ngAfterViewInit(): void {
     console.log('AFTER VIEW INIT RUNNING');
   }
 
-  ngOnDestroy(): void {
-    console.log('ON DESTROY RUNNING');
-    clearTimeout(this.interval);
-  }
+
+
+  // ngOnDestroy(): void {
+  //   console.log('ON DESTROY RUNNING');
+  //   clearTimeout(this.interval);
+  // }
 }
