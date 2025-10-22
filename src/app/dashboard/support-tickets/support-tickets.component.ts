4cda2c66ed17cd9c3de42d05bbf4f496dfc7c96b
Richard Burman <richard.burman@harperjames.co.uk>
2025-10-22 11:45:19 +0100

    Refactor ticket component to enhance details visibility toggle functionality

--- src/app/dashboard/support-tickets/support-tickets.component.ts
+++ src/app/dashboard/support-tickets/support-tickets.component.ts
@@ -1,28 +1,35 @@
 import { Component } from '@angular/core';
-import { NewTicketComponent } from "./new-ticket/new-ticket.component";
+import { NewTicketComponent } from './new-ticket/new-ticket.component';
 import { Ticket } from './ticket.model';
-import { TicketComponent } from "./ticket/ticket.component";
-
+import { TicketComponent } from './ticket/ticket.component';
 
 @Component({
   selector: 'app-support-tickets',
   standalone: true,
   imports: [NewTicketComponent, TicketComponent],
   templateUrl: './support-tickets.component.html',
-  styleUrl: './support-tickets.component.css'
+  styleUrl: './support-tickets.component.css',
 })
 export class SupportTicketsComponent {
   tickets: Ticket[] = [];
 
-  onAdd(ticketData: {title: string, text: string}) {
+  onAdd(ticketData: { title: string; text: string }) {
     const ticket: Ticket = {
       title: ticketData.title,
       request: ticketData.text,
-      id: Math.random().toString(), 
-      status: 'open'
-    }
-    
+      id: Math.random().toString(),
+      status: 'open',
+    };
+
     this.tickets.push(ticket);
   }
 
+  onCloseTicket(id: string) {
+    this.tickets = this.tickets.map((ticket) => {
+      if (ticket.id === id) {
+        return { ...ticket, status: 'closed'};
+      }
+      return ticket;
+    });
+  }
 }
